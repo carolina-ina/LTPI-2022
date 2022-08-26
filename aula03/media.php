4d456388b5a573550eacec1d5180a7a439530767
Carol <carol_n_nv@hotmail.com>
2022-08-26 11:34:50 -0300

    commit to ltpi-3ads

--- aula03/media.php
+++ aula03/media.php
@@ -4,12 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Média final</title>
 </head>
 <body>
     <?php
-
-
+    $nome = $_REQUEST["nome"];
+    $nota1 = $_REQUEST["nota1"];
+    $nota2 = $_REQUEST["nota2"];
+    $media = (($nota1 + $nota2)/ 2);
+        if($media<2){
+        echo "<p>Olá! $nome. Sua média é $media.Você foi Reprovado.</p>";
+        }elseif($media<=6){
+        echo "<p>Olá! $nome. Sua média é $media.Você foi precisa realizar a prova final.</p>";
+        }elseif($media>6){
+        echo "<p>Olá! $nome. Sua média é $media.Você foi Aprovado.</p>";
+        }
 
     ?>
 </body>
