8b2826b15238074f7f3dfebfa6486eeb637134fe
101kbps <carol_n_nv@hotmail.com>
2022-09-02 11:35:28 -0300

    commit on main

--- aula04/vetores.php
+++ aula04/vetores.php
@@ -10,13 +10,25 @@
     <?php
     $nomes =['Edna','Carol Miranda','Carol Abreu'];
     $nomes[] ='Milena';
+    $idades= array(18,30,25,18);
     $i =0;
     echo'<h2>Usando for</h2>';
     echo'<table border="1">';
+    echo'<tr><th>Nome</th><th>Idade</th></tr>';
     for($i=0;$i<=3;$i++){
 
-     echo '<tr><td>' .$nomes[$i].'</td></tr>' ;
-    
+     echo '<tr><td>'.$nomes[$i].'</td><td>'.$idades[$i].'</td></tr>' ;
+         
+    }
+    echo'</table>';
+    echo'<h2>Usando for</h2>';
+    echo'<table border="1">';
+    echo'<tr>';
+    for($i=0;$i<=3;$i++){
+
+    echo '<td>'.$nomes[$i].'</td>' ;
+    echo '<td>'.$idades[$i].'</td>';
+         
     }
     echo'</table>';
 
