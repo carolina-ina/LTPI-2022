f81557fbc82cc25b6aeb97b4446f441cb83d98d9
101kbps <carol_n_nv@hotmail.com>
2022-09-23 09:56:02 -0300

    commit to ltpi-3ads

---
+++ aula07/Funcoes.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funçoes PHP</title>
+</head>
+<body>
+    <?php
+    echo '<h2>'.'Tabuada um a um'.'</h2>';
+    function tabuada_v01($n){
+        $n = 2;
+        $i = 1;
+        echo $n." x ".$i." = ".$n*$i.'<br>';
+        $i= $i+1;
+        echo $n." x ".$i." = ".$n*$i.'<br>';
+        $i= $i+1;
+        echo $n." x ".$i." = ".$n*$i.'<br>';
+        $i= $i+1;
+        echo $n." x ".$i." = ".$n*$i.'<br>';
+        $i= $i+1;
+        echo $n." x ".$i." = ".$n*$i.'<br>';
+        $i= $i+1;
+        echo $n." x ".$i." = ".$n*$i.'<br>';
+        $i= $i+1;
+        echo $n." x ".$i." = ".$n*$i.'<br>';
+        $i= $i+1;
+        echo $n." x ".$i." = ".$n*$i.'<br>';
+        $i= $i+1;
+        echo $n." x ".$i." = ".$n*$i.'<br>';
+        $i= $i+1;
+        echo $n." x ".$i." = ".$n*$i.'<br>';
+        $i= $i+1;
+    }
+
+    tabuada_v01(2);
+    echo '<h2>'.'Tabuada com For'.'</h2>';
+    function tabuada_v02($n){
+        for($i = 0;$i <=10; $i++){
+            echo '<p>'.$n.' x '.$i.' = '.$n*$i.'</p>';
+        }
+    }
+    tabuada_v02(2);
+    ?>
+    
+</body>
+</html>
